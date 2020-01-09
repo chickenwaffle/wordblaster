3ee39f3120c9733f1d98c61f120f9bef7215e473
chickenwaffle <mattsw@outlook.com>
2020-01-08 22:49:18 -0500

    House cleaning: no longer pauses between words, some files moved into folders, dictionaries added for future smartforcer, moving towards adding functions

--- wordblaster.py
+++ wordblaster.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env monkeyrunner                                                         
 #
-# This is a Wordscapes bruteforcer for Python 2.5.3
-# Version 1.0
+# This is a Wordscapes bruteforcer for Jython 2.5.3
+# Version 1.4
 #
-# Created by Matt Wilson on
+# Created by Matt Wilson
 # January 5, 2020
 #
 #
@@ -22,13 +22,13 @@
 #    USB debugging is enabled in your phone settings.
 #
 # 2) Navigate to your platform-tools folder in your Android SDK and run "adb devices"
-#    If your device shows up in the list, then your computer recognizes your phone
-#    as an Android device.
+#    in a terminal. If your device shows up in the list, then your computer 
+#    recognizes your phone as an Android device.
 #
 # 3) Navigate to your bin folder in your Android SDK tools
 #    e.g. Android/Sdk/tools/bin
 #
-# 4) Open a SIX CHARACTER Wordscapes level on your phone.
+# 4) Open a Wordscapes level on your phone.
 #
 # 5) run "monkeyrunner wordblaster.py"
 #
@@ -63,77 +63,90 @@ parser.add_argument('-c', \
                     default=6, \
                     metavar='', \
                     help='Number of letters in circle [default=6]')
-parser.add_argument('-l', \
-                    '--min_length', \
+parser.add_argument('-m', \
+                    '--min', \
                     type=int, \
                     default=3, \
                     metavar='', \
                     help='length of words to start bruteforcing [default=3]')
+parser.add_argument('letter_bank', \
+                    type=str, \
+                    default="abcdefg", \
+                    metavar='', \
+                    help='letters in the circle, starting from the top going clockwise')
 args = parser.parse_args()
 
-# The lower the number, the faster the lines are drawn.
-# Do not go lower than .03
-speed = .06
-timeBetweenWords = .1
 
-# Constant coordinates for letters positions in the circle, listed by size
-CIRCLE6_X = [ 540,  750,  750,  540,  330, 330 ]
-CIRCLE6_Y = [1500, 1620, 1860, 1980, 1860, 1620]
 
-device = MonkeyRunner.waitForConnection()
-
-###################################################################
-# THE BRUTEFORCER
-###################################################################
-
-for letters in range (args.min_length, args.circle_size+1):
-
-    # These two variables store x,y coordinates for the line drawer.
-    # The program will insert values into these arrays after
-    #   parsing lines from the loaded permutation file.
-    draw_x = [0,0,0,0,0,0,0,0]
-    draw_y = [0,0,0,0,0,0,0,0]
+if __name__ == "__main__":
     
-    # Load the appropriate file depending on the length of word
-    filepath = '';
-    if letters == 3:
-        filepath = '3letter.txt'
-    elif letters == 4:
-        filepath = '4letter.txt'
-    elif letters == 5:
-        filepath = '5letter.txt'
-    elif letters == 6:
-        filepath = '6letter.txt'
-    f = open(filepath, "r")
-    
-    # Read all combinations as strings into an array called 'content'
-    content = f.readlines()
+    # The lower the number, the faster the lines are drawn.
+    # Do not go lower than .03
+    speed = .06
 
-    f.close()
-    
-    # For every permutation of length of letters...
-    for line in content:
-        # For every length of letter chain
-        for i in range(1, letters+1):
-            pos = i-1
+    # Constant coordinates for letters positions in the circle, listed by size
+    CIRCLE6_X = [ 540,  750,  750,  540,  330, 330 ]
+    CIRCLE6_Y = [1500, 1620, 1860, 1980, 1860, 1620]
 
-            # For every letter in the circle
-            for j in range(0, args.circle_size):
-    
-                if int(line[j]) == i:
-                    draw_x[pos] = CIRCLE6_X[j]  # TODO: Rewrite a way to
-                    draw_y[pos] = CIRCLE6_Y[j]  # TODO: work with smaller/bigger circles
+    device = MonkeyRunner.waitForConnection()
 
-        # Perform the touch screen interaction here
-        device.touch(draw_x[0], draw_y[0], MonkeyDevice.DOWN)
-        time.sleep(speed)
+    ###################################################################
+    # THE BRUTEFORCER
+    ###################################################################
+
+    for letters in range (args.min, args.circle_size+1):
+
+        # These two variables store x,y coordinates for the line drawer.
+        # The program will insert values into these arrays after
+        #   parsing lines from the loaded permutation file.
+        draw_x = [0,0,0,0,0,0,0]
+        draw_y = [0,0,0,0,0,0,0]
         
-        for i in range(1, letters):
-            device.touch(draw_x[i], draw_y[i], MonkeyDevice.MOVE)
-            time.sleep(speed)
-    
-        device.touch(draw_x[letters-1], draw_y[letters-1], MonkeyDevice.UP)
-        time.sleep(speed)
+        # Load the appropriate file depending on the length of word
+        filepath = '';
+        if letters == 3:
+            filepath = 'drawpaths/6circle_3letter.txt'
+        elif letters == 4:
+            filepath = 'drawpaths/6circle_4letter.txt'
+        elif letters == 5:
+            filepath = 'drawpaths/6circle_5letter.txt'
+        elif letters == 6:
+            filepath = 'drawpaths/6circle_6letter.txt'
+        f = open(filepath, "r")
+        
+        # Read all combinations as strings into an array called 'content'
+        content = f.readlines()
 
-        # Delay between words
-        time.sleep(timeBetweenWords)
+        f.close()
+        
+        # For every permutation of length of letters...
+        for line in content:
+            
+            # clear the output buffer
+            output = ""
+
+            # For every length of letter chain
+            for i in range(1, letters+1):
+                pos = i-1
+
+                # For every letter in the circle
+                for j in range(0, args.circle_size):
+        
+                    if int(line[j]) == i:
+                        draw_x[pos] = CIRCLE6_X[j]  # TODO: Rewrite a way to
+                        draw_y[pos] = CIRCLE6_Y[j]  # TODO: work with smaller/bigger circles
+
+                        output += args.letter_bank[j]
+
+            # Perform the touch screen interaction here
+            device.touch(draw_x[0], draw_y[0], MonkeyDevice.DOWN)
+            time.sleep(speed)
+            
+            for i in range(1, letters):
+                device.touch(draw_x[i], draw_y[i], MonkeyDevice.MOVE)
+                time.sleep(speed)
+        
+            device.touch(draw_x[letters-1], draw_y[letters-1], MonkeyDevice.UP)
+            time.sleep(speed)
+
+            print (output)
