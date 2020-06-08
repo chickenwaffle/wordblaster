651bd1e47a9501d34d4514624d9e43c1dc9b02bb
chickenwaffle <msw102394@gmail.com>
2020-06-07 23:53:34 -0400

    attempted to fix tab spacing issue

--- wordblaster.py
+++ wordblaster.py
@@ -1,17 +1,17 @@
+import time
 
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
 if __name__ == "__main__":
-    
     device = MonkeyRunner.waitForConnection()
 
-	device.touch(0, 0, MonkeyDevice.DOWN)
-	time.sleep(1)
-	
-	coord = 0;
-	while coord < 1080:
-		device.touch(coord, coord, MonkeyDevice.MOVE)
-		time.sleep(.1)
-		
-	device.touch(coord, coord, MonkeyDevice.UP)
+    device.touch(0, 0, MonkeyDevice.DOWN)
+    time.sleep(1)
 
+    coord = 0;
+
+    while coord < 1080:
+        device.touch(coord, coord, MonkeyDevice.MOVE)
+        time.sleep(.1)
+
+    device.touch(coord, coord, MonkeyDevice.UP)
